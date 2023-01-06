039a6ec919d52f9c7b72c20a7a9fde49803db01b
Mehmet <mehmetkoray_@hotmail.com>
2023-01-07 01:39:59 +0300

    Attack Fixed a bit

--- My project/Assets/Scripts/Fireball.cs
+++ My project/Assets/Scripts/Fireball.cs
@@ -6,11 +6,29 @@ public class Fireball : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
     public float bulletSpeed;
-
+    private bool isFacingUp;
 
     void Start()
     {
-        rb.velocity = transform.right * bulletSpeed;
+        if (Input.GetKey(KeyCode.W))
+        {
+            isFacingUp = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            isFacingUp = false; 
+        }
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = transform.right * bulletSpeed;
+        }
+        else if (isFacingUp)
+        {
+            rb.velocity = transform.up * bulletSpeed;
+        }
+        else if (!isFacingUp)
+        {
+            rb.velocity = -transform.up * bulletSpeed;
+        }
     }
-
 }
