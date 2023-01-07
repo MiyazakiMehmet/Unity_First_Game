2205c27162a4be7964db2ffa48e3284536d92c07
Mehmet <mehmetkoray_@hotmail.com>
2023-01-07 20:20:40 +0300

    Weapon Accuracy, Rotation and Bullet Cooldown Fixed

--- My project/Assets/Scripts/EnemyShoot.cs
+++ My project/Assets/Scripts/EnemyShoot.cs
@@ -11,20 +11,27 @@ public class EnemyShoot : MonoBehaviour
     public Transform BulletPoint;
     public GameObject BulletPrefab;
     public Transform Character;
+    private bool isFacingRight;
+
+    //Cooldown
+    private float shootCounter = 0f;
+    public float shootCooldown = 0.3f;
 
     void Update()
     {
         if (enemyFollow.detected)
         {
             Shoot();
+            Flip();
         }
-    }
+    }   
 
     void FixedUpdate()
     {
         if (enemyFollow.detected)
         {
-            float angle = Mathf.Atan2(Character.position.y, Character.position.x)
+            Vector3 targetDirection = Character.position - BulletPoint.position;
+            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x)
                 * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
@@ -32,9 +39,24 @@ public class EnemyShoot : MonoBehaviour
 
     void Shoot()
     {
-        GameObject Bullet = Instantiate(BulletPrefab, BulletPoint.position, BulletPoint.rotation);
-        rbBullet = Bullet.GetComponent<Rigidbody2D>();
-        rbBullet.AddForce(BulletPoint.up * bulletSpeed, ForceMode2D.Impulse);
+        if (Time.time > shootCounter + shootCooldown)
+        {
+            GameObject Bullet = Instantiate(BulletPrefab, BulletPoint.position, BulletPoint.rotation);
+            rbBullet = Bullet.GetComponent<Rigidbody2D>();
+            rbBullet.AddForce(BulletPoint.right * bulletSpeed, ForceMode2D.Impulse);
+            shootCounter = Time.time;
+        }
     }
 
+    void Flip()
+    {
+        if (Character.position.x >= enemyFollow.transform.position.x && isFacingRight ||
+            Character.position.x < enemyFollow.transform.position.x && !isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 FlipWeapon = transform.localScale;
+            FlipWeapon.y *= -1f;
+            transform.localScale = FlipWeapon;
+        }
+    }
 }
